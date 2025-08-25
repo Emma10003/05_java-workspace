715862637dd6011b20abca2ee770dbefdc291d46
Emma10003 <5emma10003@gmail.com>
2025-08-25 16:29:13 +0900

    operator / 삼항연산자 / stack heap

--- src/edu/op/run/OperatorRun.java
+++ src/edu/op/run/OperatorRun.java
@@ -7,6 +7,22 @@ package edu.op.run;
 import edu.op.ex.OperatorEx;
 
 public class OperatorRun {
+    // static 이 작성된 기능이나 변수이름은 메모리에서 프로그램을 사용할 때 상시사용처럼
+    // 메모리 공간을 차지하는 예약어.
+
+    /* -------------------------------------------------------------------------
+    Stack (빠른 비서같은 존재)
+    - 어디에 어떤 기능이 존재하는지 주소만 기억
+    - 기능(행동) 역할이 종료되면 버리고, 다른 기능을 수행할 준비
+    - 실질적으로 기능이 존재하는 주소만 알고 있음
+
+    Heap (창고지기)
+    - Stack 에서 주소값으로 연결되어 들어가면 존재하는 실제 물건들의 위치
+    - 누군가 클래스로 만들어진 객체를 사용하면 보관, 사용하지 않으면
+      창고 공간을 차지하는 불필요한 객체나 기능으로 판단하여 버림
+    - 실제 데이터를 저장하는 공간 (크기는 다양함)
+   -------------------------------------------------------------------------  */
+
     public static void main(String[] args) {
         /*
         * OperatorEx = 코드가 작성된 클래스 이름(=파일명)
@@ -14,13 +30,20 @@ public class OperatorRun {
         * new : 프로그램을 실행하기 위하여 새로 생성해서 사용하겠다
         * */
 
-        OperatorEx op = new OperatorEx();
+        // 자료형  변수이름 = 새로운 객체 생성
+        OperatorEx op      = new OperatorEx();
         // ex 폴더에서 만들어놓은 OperatorEx 파일을 가져와서 사용
         // 가져와서 OperatorRun 클래스 내부에서 사용할 때
         // op 라는 별칭을 붙여준 후 사용.
         // 기존에 어떤 개발자가 만들어놓은 파일을 가져와서
         // 파일에 작성된 기능이나 변수값을 활용해 코딩하려고 할 때 사용.
-        op.test();
+
+        // new OperatorEx().test();
+        op.test(); // 새로운 객체를 생성해서 객체 내부에 존재하는 기능을 사용하겠다.
         op.method1();
+        System.out.println();
+        op.method2();
+        System.out.println();
+        op.method3();
     }
 }
