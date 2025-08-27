2e6e06d3fd5b781cf404d75eccfbfbee1c4e7e57
Emma10003 <5emma10003@gmail.com>
2025-08-27 13:19:58 +0900

    캡슐화 속성 및 기능 실습

--- src/edu/oop/basic/NationRun.java
+++ src/edu/oop/basic/NationRun.java
@@ -19,9 +19,37 @@ public class NationRun {
         은행 예시로 들었을 때 창고에 직접 들어가서 돈을 저장하는 느낌.
         */
 
-        n1.setName = "홍길동";
-        n1.setAge = 25;
-        n1.setGender = '남';
-        System.out.printf("이름은 %s이고, %d세 %c성 입니다.\n", n1.getName, n1.getAge, n1.getGender);
+        n1.setName("홍길동");
+        n1.setAge(25);
+        n1.setGender('남');
+        n1.setTel("010-1234-5678");
+        n1.setAddress("서울시 강남구");
+
+        // ===== 문제 2 : 필수생성자 활용 =====
+        Nation n2 = new Nation("이영희", 17, '여',
+                "070312-4567890", "010-9876-5432", "부산시 해운대구");
+
+        // ===== 문제  3 : 필수생성자 활용 =====
+        Nation n3 = new Nation("박민수", 42, '남',
+                "820525-1987456", "010-5555-7777", "대구시 중구");
+
+        // ===== 자기소개 기능 활용해서 모든 국민들의 자기소개 확인
+        n1.introduce();
+        n2.introduce();
+        n3.introduce();
+
+        // ===== 납세의 의무 확인하여 결과 출력
+        n1.납세의의무();
+        n2.납세의의무();
+        n3.납세의의무();
+
+        // ===== 한국어 능력 및 복지혜택 확인
+        // 1. n1 국민부터 n3 국민까지 각각 한국어 능력 및 복지혜택 확인
+        n1.speakKorean();
+        n1.welfare();
+        n2.speakKorean();
+        n2.welfare();
+        n3.speakKorean();
+        n3.welfare();
     }
 }
