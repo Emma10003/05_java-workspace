45086f915ae647fea1f61f1cceebec7390044bb4
Emma10003 <5emma10003@gmail.com>
2025-09-01 17:35:14 +0900

    상속 실습 - 중복된 필드와 기능을 부모와 자식으로 분리하여 실습

---
+++ src/edu/inheritance/Grandchild.java
@@ -0,0 +1,11 @@
+package edu.inheritance;
+
+/**
+ * Parents
+ *      └─── Child
+ *              └─── Grandchild
+ */
+public class Grandchild extends Parent /*,Child 상속은 1번만 가능 - 다중상속 불가! */ {
+    
+    // GrandChild 는 Parent를 상속받거나 Child를 상속받거나 둘 중 하나만 가능
+}
