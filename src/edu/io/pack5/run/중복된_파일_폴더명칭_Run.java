4251b987dd75e69d9b20910a9d8536ff863bcd98
Emma10003 <5emma10003@gmail.com>
2025-09-10 16:44:18 +0900

    IO(입출력) - 프로필 사진 저장하기

--- src/edu/io/pack5/run/중복된_파일_폴더명칭_Run.java
+++ src/edu/io/pack5/run/중복된_파일_폴더명칭_Run.java
@@ -2,13 +2,19 @@ package edu.io.pack5.run;
 
 import edu.io.pack5.service.중복된_파일_폴더명칭_Service;
 
+import java.util.Scanner;
+
 public class 중복된_파일_폴더명칭_Run {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         중복된_파일_폴더명칭_Service service = new 중복된_파일_폴더명칭_Service();
         // service.saveImage1();
         // service.saveImage2();
         // service.saveImage3();
-        service.saveImage5();
+        // service.saveImage5();
+
+        String imgUrl = "https://cdn-icons-png.freepik.com/512/6889/6889935.png";
+        service.kakaoSave(imgUrl);
     }
 
 }
