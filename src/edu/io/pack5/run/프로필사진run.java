7e3f8dd0a6a46a7ccfdc7167cd80cb903c86572e
Emma10003 <5emma10003@gmail.com>
2025-09-10 13:11:30 +0900

    IO(입출력) - 프로필 사진 저장하기

--- src/edu/io/pack5/run/프로필사진run.java
+++ src/edu/io/pack5/run/프로필사진run.java
@@ -2,20 +2,42 @@ package edu.io.pack5.run;
 
 import edu.io.pack5.service.프로필사진service;
 
+import java.util.Scanner;
+
 public class 프로필사진run {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         프로필사진service service = new 프로필사진service();
 
+        // 1. .saveImg()
         // service.createProfileFolder("member03", "프로필사진2.png");
         // service.createProfileFolder("member04", "프로필사진2.png");
-
-        String imgUrl = "https://cdn.dailysecu.com/news/photo/202104/123449_145665_1147.png";
-
+        // String imgUrl = "https://cdn.dailysecu.com/news/photo/202104/123449_145665_1147.png";
         // service.saveImg(imgUrl);
 
+        // 2. .saveImg2()
         // 인터넷에서 이미지 주소를 복사한 후
-        // 원하는 이미지를
-        String imgUrl2 = "이미지 주소 넣어주기";
-        service.saveImg2(imgUrl2);
+        // 원하는 이미지를 저장
+        // String imgUrl2 = "https://www.fitpetmall.com/wp-content/uploads/2023/10/shutterstock_1844153299-1024x683-1.png";
+        // service.saveImg2(imgUrl2);
+
+        // 3. .saveImg3()
+        // System.out.println("저장할 사진의 명칭을 작성하세요: ");
+        // String name = sc.nextLine();
+        //
+        // String imgName = name + ".png"; // 확장자를 붙여서 저장
+        // service.saveImg3(imgName);
+
+        // 4. .saveImg4()
+        System.out.print("사진의 주소: ");
+        String imgUrl3 = sc.nextLine();
+        System.out.print("폴더이름: ");
+        String folderName = sc.nextLine();
+        System.out.print("이미지 이름: ");
+        String image = sc.nextLine();
+
+        String finalName = image + ".png";
+        service.saveImg4(folderName, finalName, imgUrl3);
     }
 }
--- src/edu/io/pack5/service/프로필사진service.java
+++ src/edu/io/pack5/service/프로필사진service.java
@@ -71,9 +71,106 @@ public class 프로필사진service {
     }
     
     // profiles/member07 내부에 복사한 이미지 저장하기
-    public void saveImg2(){
+    public void saveImg2(String imgUrl){
+        // 1. 회원폴더 경로 저장
+        Path memberFolder = Path.of("profiles", "member07");
 
+        try {
+            // 2. 폴더 생성
+            Files.createDirectories(memberFolder);
+            // 3. 저장할 파일(프로필 파일) 경로 저장
+            Path picture = Path.of("profiles", "member07", "picture01.png");
 
+            // 4. 이미지 주소에서 이미지 다운로드
+            // 인터넷 주소에서 이미지를 다운로드 할 수 있도록
+            // String 으로 가져온 문자열 주소가 인터넷 상에 존재하는 주소임을 설정하는 URL 클래스 사용
+            System.out.println("이미지 다운로드 중...");
+            URL url = new URL(imgUrl); // String 문자열에 담긴 글자가 url 주소임을 설정함.
+            // 데이터 가져오기       = 인터넷주소에서.데이터를 가져올 수 있도록 열어놓기
+            InputStream inputStream = url.openStream(); // 이미지 다운로드 할 수 있도록 통로 만들고 열기
+
+            // 5. 파일로 저장하기
+            // 인터넷 상에서 가져온 데이터 이미지를, 이미지명.png 로 복사해서 저장(.copy())
+            Files.copy(inputStream, picture);
+            inputStream.close(); // 저장 완료 후 inputStream 닫기
+
+            System.out.println("프로필 사진이 개별 폴더로 저장 완료되었습니다.");
+        } catch (IOException e) {
+            System.out.println("프로필 사진 저장 중 문제가 발생했습니다.");
+        }
+
+    }
+
+    public void saveImg3(String imgName){
+        Path memberFolder = Path.of("profiles", "member07");
+
+        try {
+            String imgUrl2 = "https://www.fitpetmall.com/wp-content/uploads/2023/10/shutterstock_1844153299-1024x683-1.png";
+
+            Files.createDirectories(memberFolder);
+            Path picture = Path.of("profiles", "member07", imgName);
+
+            URL url = new URL(imgUrl2);
+            InputStream inputStream = url.openStream();
+
+            // 이미지를 복사해서 저장한다는 의미를 쓰는 이유
+            // 웹에 있는 이미지를 그대로 복제해서 내 컴퓨터 안에 저장하는 것이기 때문
+            // 웹에 있는 이미지를 내 컴퓨터 안에 저장한다 하여 웹에 존재하는 이미지가 사라지는 것은 아니기에
+            // '웹 이미지를 복제하여 내 컴퓨터에 저장한다' => .copy() 를 사용함
+            Files.copy(inputStream, picture); // 똑같은 이름의 이미지가 존재할 때 덮어쓰기 X
+                                              // (StandardCopyOption.REPLACE_EXISTING 없기 때문)
+            inputStream.close();
+
+            System.out.println("프로필 사진이 개별 폴더로 저장 완료되었습니다.");
+        } catch (IOException e) {
+            System.out.println("프로필 사진 저장 중 문제가 발생했습니다.");
+        }
+    }
+
+    // Scanner 를 이용하여 폴더이름, 이미지이름을 사용자에게 전달받고
+    // 폴더 안에 이미지를 웹에서 복제하여 저장
+    // 확장자는 .png 사용
+    /*
+    png = 투명도를 지원하는 비손실 압축
+          로고, 아이콘 등 투명배경이 필요한 이미지 사용
+          파일 크기가 크고, 여러 번 수정해야 할 때 주로 사용함.
+    jpg = 투명도를 지원하지 않는 손실 압축
+          사진, 그라데이션, 배너 등 완성된 사진을 압축하여 데이터 크기를 줄여 사용할 때 용이
+    svg = 확장 가능한 이미지
+          이미지를 아무리 확대하거나 축소해도 절대 깨지지 않는 디지털 이미지 형식
+          수학 공식으로 이미지를 설계 (벡터 이미지 확장자)
+          화면 크기에 맞춰 매번 공식을 다시 계산 후 표현되기 때문에, 다양한 디바이스에서 유용하게 사용되는 이미지.
+          => 코드 기반 이미지!
+     */
+    // 외부 url 을 통해 이미지를 가져올 때, Java에서 접근할 수 없는 주소도 존재함.
+    // -> 사이트마다 권한에 따른 문제임.
+    //    이러한 주소에 접근하려면 추가적인 권한이 필요하기 때문에, 권한이 필요하다는 예외가 발생함.
+    //    https://upload.wikimedia.org/wikipedia/commons/thumb/3/37/Oryctolagus_cuniculus_Tasmania_2.jpg/960px-Oryctolagus_cuniculus_Tasmania_2.jpg
+    
+    public void saveImg4(String folderName, String imageName, String imgUrl){
+        Path memberFolder = Path.of("profiles", folderName);
+        // Path memberFolder = Path.of("profiles", folderName, imageName);
+
+        // String imgUrl = "https://cdn-icons-png.flaticon.com/512/3163/3163306.png";
+
+        try {
+            Files.createDirectories(memberFolder); // 폴더가 없을 경우 생성, 있으면 false로 넘어가기
+            // Files.createDirectories(memberFolder.getParent()); // 이렇게도 가능!
+            Path picture = Path.of("profiles", folderName, imageName); // 폴더 내의 이미지까지 바라보는 경로
+
+            URL url = new URL(imgUrl);
+            InputStream inputStream = url.openStream();
+            Files.copy(inputStream, picture);
+            inputStream.close();
+
+            System.out.println("사진 저장이 완료되었습니다.");
+        } catch (IOException e) {
+            System.out.println("이미지 다운로드 실패");
+            // 더 자세한 오류 정보 제공
+            if(e.getMessage().contains("403")) {
+                System.out.println("403 오류 : 위키미디어에서 접근을 거부했습니다. 함부러 들어와서 다운로드 할 수 없다네용...");
+            }
+        }
     }
 
 }
